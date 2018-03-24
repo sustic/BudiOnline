7a48df659dde4993ac3ae88cd4db551bbe3963b5
Marko Sustic <msustic91@gmail.com>
2018-03-24 10:36:56 +0100

    Added some comments for headline and subheadline element

--- src/index.php
+++ src/index.php
@@ -6,16 +6,20 @@
             width:    full-width, narrow-width
             text:     text-left, text-center, text-right
             padding:  padding-fat, padding-normal
+            background-color
         HTML:
-            h1, h2, h3, strong, b, a
+            h1, h2, h3, strong, b, i, a
         a:
             target="_blank" ***** To open link on a new tab *****
 -->
+<hr>
+<hr>
 <section class="headline-subheadline full-width text-center padding-fat">
     <div class="container" style="background-color:#f5f5f5;">
         <h1>Lorem <a href="#">ipsum dolor</a> sit amet consectetur</h1>
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et quam tenetur iure velit nemo culpa ipsa natus suscipit fuga totam, neque sequi praesentium excepturi.</p>
     </div>
 </section>
+
 
 <?php get_footer(); ?>
