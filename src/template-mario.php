04cfb2b6dfceaf24498b55de93ebc205f23035c1
Mario Jug <mario0t@gmail.com>
2018-03-30 01:42:21 +0200

    Imported icons in img folder, working on icons

--- src/template-mario.php
+++ src/template-mario.php
@@ -1,5 +1,58 @@
 <?php /* Template Name: Template Mario */ get_header(); ?>
 
+<!-- 
+    Element name: Feature with icon
+    Awailable options:
+        CSS:
+            width:    full-width, narrow-width
+            text:     text-left, text-center, text-right
+            padding:  padding-fat, padding-normal
+            background-color
+            column: column-width-25 -> 25% width
+                    column-width-33 -> 33.33% width
+                    column-width-50 -> 50% width
+                    column-width-100 -> 100% width
+        HTML:
+            h1, h2, h3, h5, strong, b, i, a
+        a:
+            target="_blank" ***** To open link on a new tab *****
+-->
+<section class="text-columns icons full-width padding-normal">
+    <div class="container" style="background-color:#f5f5f5;">
+        <div class="container-inner">
+            <div class="column-width-33 text-center">
+                <img src="<?php echo get_template_directory_uri();?>/img/icons/005-worldwide.png" alt="">
+                <h5>Lorem ipsum</h5>
+                <p>Lorem ipsum dolor sit amet, <a href="#">consectetur</a> adipiscing elit. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit, eaque veritatis natus nobis architecto, quibusdam atque quos perferendis, rerum provident iure! Blanditiis quibusdam iste facilis quo itaque asperiores nihil quaerat.</p>
+            </div> 
+            <div class="column-width-33 text-center">
+                <img src="<?php echo get_template_directory_uri();?>/img/icons/009-browser-2.png" alt="">
+                <h5>Lorem ipsum</h5>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis, et commodi dicta, asperiores quae cum quidem voluptatem quibusdam beatae officia, quia repellendus maiores. Quasi aliquid dolores, esse doloribus odio vitae.</p>
+            </div>
+            <div class="column-width-33 text-center">
+                <img src="<?php echo get_template_directory_uri();?>/img/icons/012-shield-1.png" alt="">
+                <h5>Lorem ipsum</h5>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis, et commodi dicta, asperiores quae cum quidem voluptatem quibusdam beatae officia, quia repellendus maiores. Quasi aliquid dolores, esse doloribus odio vitae.</p>
+            </div>
+            <div class="column-width-33 text-center">
+                <img src="<?php echo get_template_directory_uri();?>/img/icons/013-package.png" alt="">
+                <h5>Lorem ipsum</h5>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis, et commodi dicta, asperiores quae cum quidem voluptatem quibusdam beatae officia, quia repellendus maiores. Quasi aliquid dolores, esse doloribus odio vitae.</p>
+            </div>
+            <div class="column-width-33 text-center">
+                <img src="<?php echo get_template_directory_uri();?>/img/icons/015-list.png" alt="">
+                <h5>Lorem ipsum</h5>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis, et commodi dicta, asperiores quae cum quidem voluptatem quibusdam beatae officia, quia repellendus maiores. Quasi aliquid dolores, esse doloribus odio vitae.</p>
+            </div>
+            <div class="column-width-33 text-center">
+                <img src="<?php echo get_template_directory_uri();?>/img/icons/015-technology-3.png" alt="">
+                <h5>Lorem ipsum</h5>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis, et commodi dicta, asperiores quae cum quidem voluptatem quibusdam beatae officia, quia repellendus maiores. Quasi aliquid dolores, esse doloribus odio vitae.</p>
+            </div>
+        </div>
+    </div>
+</section>
 
 
 
