fe6af8d94c7958b2713a95b5b9ed4454be2c5819
Mario Jug <mario0t@gmail.com>
2018-04-13 00:04:09 +0200

    Created top footer

--- src/footer.php
+++ src/footer.php
@@ -1,6 +1,80 @@
 			<!-- footer -->
 			<footer class="footer" role="contentinfo">
+				<div class="footer-top full-width padding-normal ">
+					<div class="container" style="background-color: #2E4051">
+						<div class="row">
+							<div class="column">
+								<h3>Product</h3>
+								<ul>
+									<li><a href="#">Tour</a></li>
+									<li><a href="#">Services</a></li>
+									<li><a href="#">Integrations</a></li>
+									<li><a href="#">What's new</a></li>
+									<li><a href="#">Pricing</a></li>
+								</ul>
+							</div>
+							<div class="column">
+								<h3>Uses</h3>
+								<ul>
+									<li><a href="">Web Hosting</a></li>
+									<li><a href="">Domain Registrars</a></li>
+									<li><a href="">Software Developers</a></li>
+									<li><a href="">Cloud Hosting</a></li>
+									<li><a href="">Web Designers</a></li>
+									<li><a href="">ISP Billing</a></li>
+								</ul>
+							</div>
+							<div class="column">
+								<h3>Support</h3>
+								<ul>
+									<li><a href="">Technical Support</a></li>
+									<li><a href="">Community Forums</a></li>
+									<li><a href="">Tutorials & Guides</a></li>
+									<li><a href="">Documentation</a></li>
+									<li><a href="">Contact Us</a></li>									
+								</ul>
+							</div>
+							<div class="column-double">
+								<h3>Resources</h3>
+								<div class="row-inner">
+									<div class="column">
+										<ul>
+											<li><a href="">Free Tools</a></li>
+											<li><a href="">Marketplace</a></li>
+											<li><a href="">Partners</a></li>
+											<li><a href="">Download</a></li>
+											<li><a href="">Feature Requests</a></li>
+											<li><a href="">Developer Portal</a></li>
+										</ul>
+									</div>
+									<div class="column">
+										<ul>
+											<li><a href="">License Verification</a></li>
+											<li><a href="">Media Resources</a></li>
+											<li><a href="">Affiliate Program</a></li>
+											<li><a href="">Reseller Program</a></li>
+											<li><a href="">Responsible Disclosure</a></li>												
+										</ul>
+									</div>									
+								</div>
+							</div>
+							<div class="column">
+								<h3>Company</h3>
+								<ul>
+									<li><a href="">About Us</a></li>
+									<li><a href="">Blog</a></li>
+									<li><a href="">Customers</a></li>
+									<li><a href="">Jobs</a></li>
+									<li><a href="">Contact</a></li>									
+								</ul>
+							</div>							
+						</div>						
+					</div>				
+				</div>
 
+				<div class="footer-bottom" style="background-color:#283544">
+				some test second row
+				</div>
 
 			</footer>
 			<!-- /footer -->
